a41ddd8d5722e479b562e47277341ddf2b8113a5
dayan2222 <51928026+dayan2222@users.noreply.github.com>
2019-09-27 17:56:55 +0500

    Add files via upload

---
+++ MySQL Connection.py
@@ -0,0 +1,39 @@
+from tkinter import *
+import sys
+import MySQLdb
+# host,user,password,db
+conn = MySQLdb.connect("localhost","root","","codingthunder")
+if(conn):
+    print("Connection Successful")
+else:
+    print("Connection Failed")
+# connection set.......
+cur = conn.cursor()
+def SaveData():
+    f = a.get()
+    g = b.get()
+    e = c.get()
+    cur.execute("""INSERT INTO students(fname,lname,course) VALUES("%s","%s","%s")"""%(f,g,e))
+    print(f"Number of rows inserted: {cur.rowcount}")
+
+    if cur != 0:
+        conn.commit()
+    else:
+        conn.rollback()
+        conn.close()
+
+root =Tk()
+Label(root,text="First Name:").pack()
+a = Entry(root,width=15)
+a.pack()
+
+Label(root,text="Last Name:").pack()
+b = Entry(root,width=15)
+b.pack()
+
+Label(root,text="Course:").pack()
+c = Entry(root,width=15)
+c.pack()
+
+Button(root,text="Save", command=SaveData).pack()
+root.mainloop()
